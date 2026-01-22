c9850c2f0388e02cd1f398591d211e81108ce474
nyxnyx <nyxnyx@archenemy>
2026-01-23 02:27:39 +0300

    Minor update

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -2,3 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
 end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/nyxnyx/.lmstudio/bin
+# End of LM Studio CLI section
+
+
+# =======
+# ALIASES
+# =======
+
+alias ports="netstat -tulanp"
