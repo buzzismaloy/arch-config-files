5ea7a3199cdd1225fa7b07668ff02bcb65ea3ba7
nyxnyx <nyxnyx@archenemy>
2026-02-07 21:26:57 +0300

    Added some more options to smart launcher

--- src/wofi/scripts/smart-launcher.sh
+++ src/wofi/scripts/smart-launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-choice=$(printf "Apps\nCalc\nReload waybar\nLock Screen\nScreenshot" | \
-wofi --dmenu --location top_left --width 170 --height 230 --xoffset 10 --prompt "Launcher")
+choice=$(printf "Apps\nCalc\nReload waybar\nLock Screen\nScreenshot\nRandom Wallpaper\nPower Menu" | \
+wofi --dmenu --location top_left --width 200 --height 230 --xoffset 10 --prompt "Launcher")
 
 case "$choice" in
   "Apps") ~/.config/wofi/scripts/launcher.sh ;;
@@ -9,5 +9,7 @@ case "$choice" in
   "Reload waybar") ~/.config/wofi/scripts/reload_waybar.sh ;;
   "Lock Screen") hyprlock ;;
   "Screenshot") hyprshot -m output -o $HOME/Pictures/Screenshots ;;
+  "Random Wallpaper") ~/bin/random_wallpaper.sh ;;
+  "Power Menu") wlogout -b 2 ;;
   #Emoji) wofi-emoji ;;
 esac
